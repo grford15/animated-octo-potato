04df6fca25889d46846f0d3497080f673c8f044c
Greg <g.rutherford10@gmail.com>
2020-03-16 15:37:00 +0000

    add basic components

---
+++ src/components/Stories.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Stories.css';
+
+import Story from './Story';
+
+const Stories = ({ stories }) => (
+  <div className="stories">
+    {(stories || []).map(story => (
+      <Story key={story.objectId} story={story} />
+    ))}
+  </div>
+);
+
+export default Stories;
