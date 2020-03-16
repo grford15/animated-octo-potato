11f1ce5bb8228ae53c9c845132aee3188aaeaa35
Greg <g.rutherford10@gmail.com>
2020-03-16 16:00:04 +0000

    add basic styling to components

--- src/components/Stories.jsx
+++ src/components/Stories.jsx
@@ -5,6 +5,12 @@ import Story from './Story';
 
 const Stories = ({ stories }) => (
   <div className="stories">
+    <div className="stories-header">
+      <span>Title</span>
+      <span>Author</span>
+      <span>Number of Comments</span>
+      <span>Points</span>
+    </div>
     {(stories || []).map(story => (
       <Story key={story.objectId} story={story} />
     ))}
