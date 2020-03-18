fb24c83efad4ddb42d3abaa83c919d72df104aaa
Greg <g.rutherford10@gmail.com>
2020-03-18 14:53:23 +0000

    add function to return non-archived stories

--- src/index.jsx
+++ src/index.jsx
@@ -3,11 +3,12 @@ import ReactDOM from 'react-dom';
 import App from './components/App';
 import store from './store';
 import { STORY_ARCHIVE } from './constants/actionTypes';
+import { getReadableStories } from './selectors/story';
 import './index.css';
 
 ReactDOM.render(
   <App
-    stories={store.getState().storyState}
+    stories={getReadableStories(store.getState())}
     onArchive={id => store.dispatch({ type: STORY_ARCHIVE, id })}
   />,
   document.getElementById('root'),
