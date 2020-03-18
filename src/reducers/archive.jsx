df21178f6ef27205a4d04a3579fcd889cd7a8c41
Greg <g.rutherford10@gmail.com>
2020-03-18 14:42:55 +0000

    add archive story action

--- src/reducers/archive.jsx
+++ src/reducers/archive.jsx
@@ -1,10 +1,17 @@
+import { STORY_ARCHIVE } from '../constants/actionTypes';
+
 const INITIAL_STATE = [];
+
+const applyArchiveStory = (state, action) => [...state, action.id];
 
 export default function archiveReducer(
   state = INITIAL_STATE,
   action,
 ) {
   switch (action.type) {
+    case STORY_ARCHIVE: {
+      return applyArchiveStory(state, action);
+    }
     default:
       return state;
   }
